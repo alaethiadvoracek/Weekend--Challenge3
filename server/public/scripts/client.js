41950827486901517c755e5ea60d7c94a9782a8e
Alaethia <truthcharis@gmail.com>
2018-01-13 12:17:58 -0600

    added some things to client, html, css

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,21 @@
 console.log('client.js sourced');
 
-// $( document ).ready(onLoad);
+$( document ).ready(onLoad);
 
+function onLoad (){
+console.log('jq');
+$('#submitTask').on('click', addNewTask);
+
+
+}//end on load 
+
+function addNewTask(){
+$.ajax({
+    method: 'GET', 
+    url: '/task',
+    success: function(response){
+        console.log('Get request response:', response);
+    }
+});//end ajax get task to database 
+
+}//end add new task to list 

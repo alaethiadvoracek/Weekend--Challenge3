21912141f34de95ece73ce9433f0ccb62fc5f9f1
Alaethia <truthcharis@gmail.com>
2018-01-14 21:18:12 -0600

    added to database

--- to_do_list.sql
+++ to_do_list.sql
@@ -5,4 +5,20 @@ CREATE TABLE task (
 	day_of_the_week VARCHAR(20),
 	complete_task VARCHAR(3) 
 );
+-- started this part but did not get to it
 
+CREATE TABLE category (
+	id SERIAL PRIMARY KEY,
+	category_name VARCHAR(30)
+
+);
+
+INSERT INTO "category" ("category_name")
+VALUES ('Chores'),('School'),('Business'),('Misc.');
+
+CREATE TABLE task_category (
+	id SERIAL PRIMARY KEY, 
+	category_id INT REFERENCES "category", 
+	task_id INT REFERENCES "task"
+
+);
